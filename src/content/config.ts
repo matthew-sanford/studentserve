5991e4d74343209ac02b0b1ac3bb6d33ae719e10
shakenbird <matthewsanford35@gmail.com>
2024-08-11 16:22:54 -0700

    updated studentserve info

--- src/content/config.ts
+++ src/content/config.ts
@@ -2,7 +2,7 @@
 import { z, defineCollection } from 'astro:content';
 
 // 2. Define your collection(s)
-const blogCollection = defineCollection({
+const listingsCollection = defineCollection({
   schema: z.object({
     draft: z.boolean(),
     title: z.string(),
@@ -12,28 +12,14 @@ const blogCollection = defineCollection({
       alt: z.string(),
     }),
     publishDate: z.string().transform(str => new Date(str)),
-    author: z.string().default('Astroship'),
+    author: z.string().default('StudentServe Listings'),
     category: z.string(),
     tags: z.array(z.string()),
-  }),
-});
-
-const teamCollection = defineCollection({
-  schema: z.object({
-    draft: z.boolean(),
-    name: z.string(),
-    title: z.string(),
-    avatar: z.object({
-      src: z.string(),
-      alt: z.string(),
-    }),
-    publishDate: z.string().transform(str => new Date(str)),
   }),
 });
 
 // 3. Export a single `collections` object to register your collection(s)
 //    This key should match your collection directory name in "src/content"
 export const collections = {
-  'blog': blogCollection,
-  'team': teamCollection,
+  'listings': listingsCollection,
 };
